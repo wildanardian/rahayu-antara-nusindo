fa55cb0203762b47ce0a46df4f286b6ac8a1f438
Dedy Rahmat <acesonix@gmail.com>
2023-07-17 10:58:50 +0700

    add ofp model

---
+++ models/ofp.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const {Schema} = mongoose;
+
+const ofpSchema = new Schema({
+    title: {type: String, required: true},
+    image: {type: String, required: true},
+    description: {type: String, required: true}
+}, {timestamps: true})
+
+const ofp = mongoose.model('ofp', ofpSchema);
+
+module.exports  = ofp;
