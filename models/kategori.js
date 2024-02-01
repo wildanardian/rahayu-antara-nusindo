5dff2f01b47fad8e666db99c0e67b229bc13b347
JorLojor <nugas123man@gmail.com>
2024-02-01 23:59:48 +0700

    kategori

--- models/kategori.js
+++ models/kategori.js
@@ -4,8 +4,7 @@ const {Schema} = mongoose;
 const kategoriSchema = new Schema({
     nama : {type: String , required: true},
     ofp : [{type: Schema.Types.ObjectId, ref: 'ofp'}]
-},{timestamps: true})
-
+},{timestamps: true});
 
 const kategori = mongoose.model('kategori',kategoriSchema);
 
